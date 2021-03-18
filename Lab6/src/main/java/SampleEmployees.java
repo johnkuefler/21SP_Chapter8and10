9c7a091766ca0a835a0688e5bd4d1e29f0d25cbc
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-03-18 13:22:47 -0500

    2nd Commit

--- Lab6/src/main/java/SampleEmployees.java
+++ Lab6/src/main/java/SampleEmployees.java
@@ -11,16 +11,20 @@
 public class SampleEmployees {
     
     public static void main(String[] args) {
-    Employee Susan = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
-    Employee Mark = new Employee("Mark Jones", 39119, "IT", "Programmer");
-    Employee Joy =new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
+    Employee Susan = new Employee("Susan Meyers ", 47899, " Accounting ", "Vice President");
+    Employee Mark = new Employee("Mark Jones ", 39119, " IT ", "Programmer ");
+    Employee Joy =new Employee("Joy Rogers ", 81774, " Manufacturing ", "Engineer");
     
-    
-    System.out.println(Susan.getName());
-    System.out.println(Susan.getIDnumber());
-    System.out.println(Susan.getDepartment());
-    System.out.println(Susan.getPosition());
+   EmployeeInfoPrint(Susan);
+   EmployeeInfoPrint(Mark);
+   EmployeeInfoPrint(Joy);
     }
-    
+    public static void EmployeeInfoPrint(Employee Emp){
+    String EmpName = Emp.getName();
+    int EmpID = Emp.getIDnumber();
+    String EmpDepartment = Emp.getDepartment();
+    String EmpPosition = Emp.getPosition();
+    System.out.println(EmpName + EmpID + EmpDepartment + EmpPosition);
+    }
   
 }
