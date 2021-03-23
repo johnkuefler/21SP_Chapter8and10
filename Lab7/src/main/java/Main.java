068999bed7ea13f297584cb482541a45d7075ac3
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-03-23 09:11:27 -0500

    1st Commit

---
+++ Lab7/src/main/java/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+   import java.util.Scanner;
+/**
+ *
+ * @author Jacks
+ */
+public class Main {
+ 
+
+
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your age");
+
+        int age = keyboard.nextInt();
+
+        double lifeInsuranceCost = calculateLifeInsurance(age);
+        
+        System.out.println("Your estimate is " + lifeInsuranceCost);
+    }
+    
+    public static double calculateLifeInsurance(int age) {
+        double insuranceCost = (5 * age) + 300;
+        return insuranceCost;
+    }
+}
+
+
